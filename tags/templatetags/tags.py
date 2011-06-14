9eb18c422429023be20742064dc74b24f8bda284
Han Zhang <zhanghan.simon@gmail.com>
2011-06-14 00:18:05 -0400

    modified vega configs

--- tags/templatetags/tags.py
+++ tags/templatetags/tags.py
@@ -7,11 +7,7 @@ def active(request, pattern):
     import re
     if '/' == pattern:
         if '/'  == request.path:
-            print 'pattern',pattern
-            print 'path',request.path
             return 'class="on"'
     elif re.match(pattern, request.path):
-        print 'pattern',pattern
-        print 'path',request.path
         return 'class="on"'
     return ''

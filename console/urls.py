e4a36ec403e9d458886da20b1284d6ac649af5a5
Han Zhang <zhanghan.simon@gmail.com>
2011-06-16 11:34:32 -0400

    url mapping fixed

--- console/urls.py
+++ console/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('console.views',
     (r'^upload/$', 'upload_file'),
     (r'uploadshare/$', 'upload_share'),
-    (r'getCitiesByProvince', 'get_cities_by_province'),
+    #(r'getCitiesByProvince', 'get_cities_by_province'),
 )

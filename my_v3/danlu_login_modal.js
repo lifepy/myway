c77074ef0b93a299a86be3ed8ec67941a479803e
Danlu Rong <rongdanlu@gmail.com>
2011-07-02 22:44:56 -0400

    register_modal

---
+++ my_v3/danlu_login_modal.js
@@ -0,0 +1,39 @@
+		function updateTips( t ) {
+			$('.validateTips')
+				.text( t )
+				.addClass( "ui-state-highlight" );
+			setTimeout(function() {
+				$('.validateTips').removeClass( "ui-state-highlight", 111500 );
+			}, 500 );
+		}
+
+		function checkLength( o, n, min, max ) {
+			if ( o.val().length > max || o.val().length < min ) {
+				o.addClass( "ui-state-error" );				
+				updateTips( "Length of " + n + " must be between " +
+					min + " and " + max + "." ); 
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		function checkRegexp( o, regexp, n ) {
+			if ( !( regexp.test( o.val() ) ) ) {
+				o.addClass( "ui-state-error" );
+				updateTips( n );
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		function checkRetype( o1,o2) {
+			if ( !(o1.val()==o2.val()) ) {
+				o2.addClass( "ui-state-error" );
+				updateTips( "Retyped password is not consistant with the password!" );
+				return false;
+			} else {
+				return true;
+			}
+		}
